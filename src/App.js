5811a0729755d1315807936fa4696b5b82155ec3
JunJI97 <102650903+JunJI97@users.noreply.github.com>
2022-04-04 11:46:49 +0900

    banner_added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    count : 0,
+  };
+  
+  handleChange = () => {
+    this.setState({
+      count : this.state.count + 1,
+    });
+  };
+
+  render() {
+    return (
+    <div className='App'>
+      <h3>index Props</h3>
+      <div className='props'>
+        <span>{this.props.message}</span>
+      </div>
+
+      <h3>State</h3>
+      <div className='state'>
+        {this.state.count}
+        <button onClick={this.handleChange}>click me !</button>
+      </div>
+
+      <h3>App Props</h3>
+      <div className='inside-app-props'>
+        <InsideApp
+        count = {this.state.count}
+        handleChange={this.handleChange} />
+      </div>
     </div>
-  );
+    );
+  }
+}
+
+class InsideApp extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.count}
+        <button onClick={this.props.handleChange}>click Me!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
